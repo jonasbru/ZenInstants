1a1c467244b0323e2c6a9c151e90ea760fb0e8da
jonas <jonasbru@wanadoo.fr>
2012-12-11 00:14:43 +0100

    work on favorites, again

--- src/com/majomi/zeninstants/settingscontroller/FavoritesManager.java
+++ src/com/majomi/zeninstants/settingscontroller/FavoritesManager.java
@@ -26,49 +26,53 @@ public class FavoritesManager {
 
 	}
 	//******** END Singleton ***********
-	
+
 	private ArrayList<Favorite> favoritesNotSended = new ArrayList<Favorite>();
 
 	public void addFavorite(MessageTextEntity entity) {
 		entity.setFavorite(true);
-		
+
+		boolean founded = false;
 		for(Favorite f : this.favoritesNotSended) {
 			if(f.getId() == entity.getId()) {
-				this.favoritesNotSended.remove(f);
+				f.setFav(true);
+				founded = true;
 				break;
 			}
 		}
-		
-		Favorite f = new Favorite();
-		f.setId(entity.getId());
-		f.setFav(true);
-		if(entity instanceof MessageImageEntity) {
-			f.setType("i");
-		} else if(entity instanceof MessageSoundEntity) {
-			f.setType("s");
-		} else if(entity instanceof MessageVideoEntity) {
-			f.setType("v");
-		} else {
-			f.setType("t");
+
+		if(!founded) {
+			Favorite f = new Favorite();
+			f.setId(entity.getId());
+			f.setFav(true);
+			if(entity instanceof MessageImageEntity) {
+				f.setType("i");
+			} else if(entity instanceof MessageSoundEntity) {
+				f.setType("s");
+			} else if(entity instanceof MessageVideoEntity) {
+				f.setType("v");
+			} else {
+				f.setType("t");
+			}
+
+			favoritesNotSended.add(f);
 		}
-		
-		favoritesNotSended.add(f);
-		
+
 		trySendFav();
 	}
 
 	public void removeFavorite(MessageTextEntity entity) {
 		entity.setFavorite(false);
 	}
-	
+
 	public void loadFavorites() {
 	}
 
 	private void trySendFav() {
 		if(Utils.isNetworkAvailable()) {
-			
+
 		}
-		
+
 	}
 
 }
@@ -79,14 +83,14 @@ public class FavoritesManager {
  */
 class Favorite implements Serializable{
 	private static final long serialVersionUID = -340687877983027455L;
-	
+
 	private long id;
 	private String type;
 	private boolean fav;
-	
-	
-	
-	
+
+
+
+
 	public long getId() {
 		return id;
 	}
