15789e5038b8b51614783dac217411ffc56253a7
Michael FAGNO <michael.fagno@gmail.com>
2012-12-10 21:33:35 +0100

    Text updated in message

--- src/com/majomi/zeninstants/MessageSoundActivity.java
+++ src/com/majomi/zeninstants/MessageSoundActivity.java
@@ -2,9 +2,11 @@ package com.majomi.zeninstants;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
+import com.majomi.zeninstants.settingscontroller.HistorialManager;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MessageSoundActivity extends SherlockActivity {
 
@@ -14,6 +16,13 @@ public class MessageSoundActivity extends SherlockActivity {
 		setContentView(R.layout.activity_message_sound);
 		getWindow().addFlags( WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
 		MessageButtonManager btnHandler = new MessageButtonManager(this);
+		fillView();
+	}
+	
+	public void fillView(){
+		int msgId = getIntent().getExtras().getInt("MESSAGE_ID");
+		TextView text = (TextView) this.findViewById(R.id.message_text);
+		text.setText(HistorialManager.getHistorialManager().getMessage(msgId).getText());
 	}
 
 	@Override
--- src/com/majomi/zeninstants/MessageVideoActivity.java
+++ src/com/majomi/zeninstants/MessageVideoActivity.java
@@ -2,9 +2,11 @@ package com.majomi.zeninstants;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
+import com.majomi.zeninstants.settingscontroller.HistorialManager;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MessageVideoActivity extends SherlockActivity {
 
@@ -14,8 +16,15 @@ public class MessageVideoActivity extends SherlockActivity {
 		setContentView(R.layout.activity_message_video);
 		getWindow().addFlags( WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
 		MessageButtonManager btnHandler = new MessageButtonManager(this);
+		fillView();
 	}
 
+	public void fillView(){
+		int msgId = getIntent().getExtras().getInt("MESSAGE_ID");
+		TextView text = (TextView) this.findViewById(R.id.message_text);
+		text.setText(HistorialManager.getHistorialManager().getMessage(msgId).getText());
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getSupportMenuInflater().inflate(R.menu.activity_message_video, menu);
