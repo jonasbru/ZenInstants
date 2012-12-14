b6a56cedadab630aa0be2ce9a17d95b565d1027f
Michael FAGNO <michael.fagno@gmail.com>
2012-12-14 22:11:57 +0100

    Home button working in settings

--- src/com/majomi/zeninstants/TabsActivity.java
+++ src/com/majomi/zeninstants/TabsActivity.java
@@ -123,5 +123,19 @@ public class TabsActivity extends SherlockFragmentActivity {
 	    }
 			
 	}
+    
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+
+        int itemId = item.getItemId();
+        switch (itemId) {
+        case android.R.id.home:
+        	finish();
+            break;
+
+        }
+
+        return true;
+    }
 
 }
